347d537ada32d065c4d91ef7226d3e7c17711917
0lym <aguineaolymm@gmail.com>
2024-02-09 01:20:50 +0800

    Added fonts, style for links. Put finished header

--- 0lymWebsite/src/App.jsx
+++ 0lymWebsite/src/App.jsx
@@ -1,6 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/esm/Container";
+import Header from "./component/Header";
+import "./styles/colorpalette.css";
+import "./styles/app.css";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Container></Container>
+    </>
+  );
 }
 
 export default App;
