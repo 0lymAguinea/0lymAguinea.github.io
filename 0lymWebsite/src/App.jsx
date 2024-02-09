0e5f5c5dee293238c87323209069f558e61be40f
0lym <aguineaolymm@gmail.com>
2024-02-09 20:26:50 +0800

    Added sections

--- 0lymWebsite/src/App.jsx
+++ 0lymWebsite/src/App.jsx
@@ -1,6 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/esm/Container";
 import Header from "./component/Header";
+import AboutMe from "./component/AboutMe";
+import TechStack from "./component/TechStack";
+import Projects from "./component/Projects";
+import Contact from "./component/Contact";
 import "./styles/colorpalette.css";
 import "./styles/app.css";
 
@@ -8,7 +12,12 @@ function App() {
   return (
     <>
       <Header />
-      <Container></Container>
+      <Container>
+        <AboutMe />
+        <TechStack />
+        <Projects />
+        <Contact />
+      </Container>
     </>
   );
 }
