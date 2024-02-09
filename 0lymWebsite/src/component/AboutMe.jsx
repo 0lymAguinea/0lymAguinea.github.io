b403c9fc4321198dd3c827381f8b1d4e11144f4d
0lym <aguineaolymm@gmail.com>
2024-02-09 18:38:49 +0800

    Edit text

--- 0lymWebsite/src/component/AboutMe.jsx
+++ 0lymWebsite/src/component/AboutMe.jsx
@@ -4,10 +4,11 @@ import "../styles/aboutMe.css";
 function PersonalInfo() {
   return (
     <>
-      <h2 className="fs-2">Hi. I'm </h2>
-      <h1 className="name fs-1 fw-semibold">Olym M. Aguinea</h1>
       <p className="fs-2">
-        I am currently doing self-study to become{" "}
+        Hi. I'm <span className="name fs-2 fw-semibold">Olym M. Aguinea</span>{" "}
+      </p>
+      <p className="fs-2">
+        I am currently self-studying to become{" "}
         <span className="jobTitle">a front-end web developer.</span>
       </p>
     </>
