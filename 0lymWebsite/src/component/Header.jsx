72929e76c0c8d7aac58853dfabdf78e3a4fdd169
0lym <aguineaolymm@gmail.com>
2024-02-11 01:28:26 +0800

    Added scrollTo function util

--- 0lymWebsite/src/component/Header.jsx
+++ 0lymWebsite/src/component/Header.jsx
@@ -2,6 +2,8 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+import { scrollToProjects, scrollToContact } from "../utils/scrollTo";
+
 function Header() {
   return (
     <Navbar expand="lg" className="navbar-dark">
@@ -9,16 +11,19 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto" />
         <Navbar.Collapse id="basic-navbar-nav" className="ms-auto">
           <Nav className="ms-auto align-items-center">
-            <Nav.Link href="#home" className="primaryColor linksHover fs-1">
-              Home
-            </Nav.Link>
-            <Nav.Link href="#projects" className="plainColor linksHover fs-2">
+            <Nav.Link className="primaryColor linksHover fs-1">Home</Nav.Link>
+            <Nav.Link
+              href="#"
+              className="plainColor linksHover fs-2"
+              onClick={scrollToProjects}
+            >
               Projects
             </Nav.Link>
-            <Nav.Link href="#resume" className="plainColor linksHover fs-2">
-              Résumé
-            </Nav.Link>
-            <Nav.Link href="#contact" className="plainColor linksHover fs-2">
+            <Nav.Link
+              href="#"
+              className="plainColor linksHover fs-2"
+              onClick={scrollToContact}
+            >
               Contact
             </Nav.Link>
           </Nav>
